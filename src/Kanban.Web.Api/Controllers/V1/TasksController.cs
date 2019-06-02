c7908fa333fb87df1287643a6b93707cbdfabb48
MariuszCh <ja@mariusz.ch>
2019-06-02 16:45:14 +0200

    Refactoring

--- src/Kanban.Web.Api/Controllers/V1/TasksController.cs
+++ src/Kanban.Web.Api/Controllers/V1/TasksController.cs
@@ -1,17 +1,13 @@
 ﻿using Kanban.Common;
 using Kanban.Web.Api.InquiryProcessing;
 using Kanban.Web.Api.Models;
-using Kanban.Web.Common;
 using Kanban.Web.Common.Routing;
-using System.Net.Http;
 using System.Web.Http;
-using Kanban.Web.Common.Validation;
 using System.Web.Http.Cors;
 
 namespace Kanban.Web.Api.Controllers.V1
 {
-    [ApiVersion1RoutePrefix("tasks")]
-    //[UnitOfWorkActionFilter]
+	[ApiVersion1RoutePrefix("tasks")]
     [Authorize(Roles = Constants.RoleNames.User)]
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class TasksController : ApiController
@@ -23,41 +19,11 @@ namespace Kanban.Web.Api.Controllers.V1
             _taskByIdInquiryProcessor = taskByIdInquiryProcessor;
         }
 
-        //[Route("", Name = "AddTaskRoute")]
-        //[HttpPost]
-        //[ValidateModel]
-        //[Authorize(Roles = Constants.RoleNames.Admin)]
-        //public IHttpActionResult AddTask(HttpRequestMessage requestMessage, NewTask newTask)
-        //{
-        //    var task = _addTaskMaintenanceProcessor.AddTask(newTask);
-        //    var result = new TaskCreatedActionResult(requestMessage, task);
-        //    return result;
-        //}
-
         [Route("{id:long}", Name = "GetTaskRoute")]
         public Task GetTask(long id)
         {
             var task = _taskByIdInquiryProcessor.GetTask(id);
             return task;
         }
-
-        //[Route("", Name = "GetTasksRoute")]
-        //public PagedDataInquiryResponse<Task> GetTasks(HttpRequestMessage requestMessage)
-        //{
-        //    var request = _pagedDataRequestFactory.Create(requestMessage.RequestUri);
-        //    var tasks = _allTasksInquiryProcessor.GetTasks(request);
-        //    return tasks;
-        //}
-
-        //[Route("{id:long}", Name = "UpdateTaskRoute")]
-        //[HttpPut]
-        //[HttpPatch]
-        //[ValidateTaskUpdateRequest]
-        //[Authorize(Roles = Constants.RoleNames.SuperUser)]
-        //public Task UpdateTask(long id, [FromBody] object updatedTask)
-        //{
-        //    var task = _updateTaskMaintenanceProcessor.UpdateTask(id, updatedTask);
-        //    return task;
-        //}
     }
 }
--- src/Kanban.Web.Api/Controllers/V1/UsersController.cs
+++ src/Kanban.Web.Api/Controllers/V1/UsersController.cs
@@ -1,17 +1,13 @@
 ﻿using Kanban.Common;
 using Kanban.Web.Api.InquiryProcessing;
 using Kanban.Web.Api.Models;
-using Kanban.Web.Common;
 using Kanban.Web.Common.Routing;
-using System.Net.Http;
 using System.Web.Http;
-using Kanban.Web.Common.Validation;
 using System.Web.Http.Cors;
 
 namespace Kanban.Web.Api.Controllers.V1
 {
-    [ApiVersion1RoutePrefix("Users")]
-    //[UnitOfWorkActionFilter]
+	[ApiVersion1RoutePrefix("Users")]
     [Authorize(Roles = Constants.RoleNames.User)]
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class UsersController : ApiController
@@ -23,41 +19,11 @@ namespace Kanban.Web.Api.Controllers.V1
             _userByIdInquiryProcessor = userByIdInquiryProcessor;
         }
 
-        //[Route("", Name = "AddUserRoute")]
-        //[HttpPost]
-        //[ValidateModel]
-        //[Authorize(Roles = Constants.RoleNames.Admin)]
-        //public IHttpActionResult AddUser(HttpRequestMessage requestMessage, NewUser newUser)
-        //{
-        //    var User = _addUserMaintenanceProcessor.AddUser(newUser);
-        //    var result = new UserCreatedActionResult(requestMessage, User);
-        //    return result;
-        //}
-
         [Route("{id:long}", Name = "GetUserRoute")]
         public User GetUser(long id)
         {
             var user = _userByIdInquiryProcessor.GetUser(id);
             return user;
         }
-
-        //[Route("", Name = "GetUsersRoute")]
-        //public PagedDataInquiryResponse<User> GetUsers(HttpRequestMessage requestMessage)
-        //{
-        //    var request = _pagedDataRequestFactory.Create(requestMessage.RequestUri);
-        //    var Users = _allUsersInquiryProcessor.GetUsers(request);
-        //    return Users;
-        //}
-
-        //[Route("{id:long}", Name = "UpdateUserRoute")]
-        //[HttpPut]
-        //[HttpPatch]
-        //[ValidateUserUpdateRequest]
-        //[Authorize(Roles = Constants.RoleNames.SuperUser)]
-        //public User UpdateUser(long id, [FromBody] object updatedUser)
-        //{
-        //    var User = _updateUserMaintenanceProcessor.UpdateUser(id, updatedUser);
-        //    return User;
-        //}
     }
 }
